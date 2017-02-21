6f3b20749627d5e46575e936a89d498f848cd990
axb <alex.barakhtyan@gmail.com>
2017-02-21 19:16:57 +0300

    fmt

--- src/log/scoped_timer.h
+++ src/log/scoped_timer.h
@@ -9,35 +9,33 @@
 #ifndef scoped_timer_h
 #define scoped_timer_h
 
-#include <iostream>
 #include <chrono>
+#include <iostream>
 
-class scoped_timer_t
-{
-   std::string _caption;
-   std::chrono::high_resolution_clock::time_point _start;
-   int _count;
+class scoped_timer_t {
+  std::string _caption;
+  std::chrono::high_resolution_clock::time_point _start;
+  int _count;
+
 public:
-   scoped_timer_t( std::string caption ) : _caption( caption ), _count( -1 ) {
-      _start = std::chrono::high_resolution_clock::now();
-      std::cout << std::endl << _caption << " ------------ " << std::endl;
-   }
-   
-   void setCount( int  cnt ) {
-      _count = cnt;
-   }
-   
-   ~scoped_timer_t() {
-      auto end = std::chrono::high_resolution_clock::now();
-      std::chrono::duration<double> diff = end - _start;
-      std::cout << std::endl << "took : " << diff.count() << " seconds" << std::endl;
-      if ( _count != -1 ) {
-         std::cout << "made : " << _count << " records, " << std::endl
-         << "perf : " << _count / diff.count() << " recs/sec " << std::endl;
-      }
-   }
+  scoped_timer_t(std::string caption) : _caption(caption), _count(-1) {
+    _start = std::chrono::high_resolution_clock::now();
+    std::cout << std::endl << _caption << " ------------ " << std::endl;
+  }
+
+  void setCount(int cnt) { _count = cnt; }
+
+  ~scoped_timer_t() {
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end - _start;
+    std::cout << std::endl
+              << "took : " << diff.count() << " seconds" << std::endl;
+    if (_count != -1) {
+      std::cout << "made : " << _count << " records, " << std::endl
+                << "perf : " << _count / diff.count() << " recs/sec "
+                << std::endl;
+    }
+  }
 };
-
-
 
 #endif /* scoped_timer_h */
